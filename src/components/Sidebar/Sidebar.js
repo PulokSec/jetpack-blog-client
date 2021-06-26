43e8a138cabb69942539d4ebc5d5871da8780fc6
PulokSec <riopulok@gmial.com>
2021-06-26 23:45:01 +0600

    jetpack blog

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,94 @@
+import React from 'react'
+
+export default function Sidebar() {
+  return (
+            <div class="w-3/12 hidden xl:block">
+
+                <div class="w-full bg-white shadow-sm rounded-sm p-4 ">
+                    <h3 class="text-xl font-semibold text-gray-700 mb-3 font-roboto">Categories</h3>
+                    <div class="space-y-2">
+                        <a href="#"
+                            class="flex leading-4 items-center text-gray-700 font-semibold text-sm uppercase transition hover:text-blue-500">
+                            <span class="mr-2">
+                                <i class="far fa-folder-open"></i>
+                            </span>
+                            <span>Beauty</span>
+                            <p class="ml-auto font-normal">(12)</p>
+                        </a>
+                        <a href="#"
+                            class="flex leading-4 items-center text-gray-700 font-semibold text-sm uppercase transition hover:text-blue-500">
+                            <span class="mr-2">
+                                <i class="far fa-folder-open"></i>
+                            </span>
+                            <span>Business</span>
+                            <p class="ml-auto font-normal">(15)</p>
+                        </a>
+                        <a href="#"
+                            class="flex leading-4 items-center text-gray-700 font-semibold text-sm uppercase transition hover:text-blue-500">
+                            <span class="mr-2">
+                                <i class="far fa-folder-open"></i>
+                            </span>
+                            <span>Fashion</span>
+                            <p class="ml-auto font-normal">(5)</p>
+                        </a>
+                        <a href="#"
+                            class="flex leading-4 items-center text-gray-700 font-semibold text-sm uppercase transition hover:text-blue-500">
+                            <span class="mr-2">
+                                <i class="far fa-folder-open"></i>
+                            </span>
+                            <span>Food</span>
+                            <p class="ml-auto font-normal">(10)</p>
+                        </a>
+                        <a href="#"
+                            class="flex leading-4 items-center text-gray-700 font-semibold text-sm uppercase transition hover:text-blue-500">
+                            <span class="mr-2">
+                                <i class="far fa-folder-open"></i>
+                            </span>
+                            <span>Learn</span>
+                            <p class="ml-auto font-normal">(3)</p>
+                        </a>
+                        <a href="#"
+                            class="flex leading-4 items-center text-gray-700 font-semibold text-sm uppercase transition hover:text-blue-500">
+                            <span class="mr-2">
+                                <i class="far fa-folder-open"></i>
+                            </span>
+                            <span>Music</span>
+                            <p class="ml-auto font-normal">(7)</p>
+                        </a>
+                        <a href="#"
+                            class="flex leading-4 items-center text-gray-700 font-semibold text-sm uppercase transition hover:text-blue-500">
+                            <span class="mr-2">
+                                <i class="far fa-folder-open"></i>
+                            </span>
+                            <span>Nature</span>
+                            <p class="ml-auto font-normal">(0)</p>
+                        </a>
+                        <a href="#"
+                            class="flex leading-4 items-center text-gray-700 font-semibold text-sm uppercase transition hover:text-blue-500">
+                            <span class="mr-2">
+                                <i class="far fa-folder-open"></i>
+                            </span>
+                            <span>People</span>
+                            <p class="ml-auto font-normal">(13)</p>
+                        </a>
+                        <a href="#"
+                            class="flex leading-4 items-center text-gray-700 font-semibold text-sm uppercase transition hover:text-blue-500">
+                            <span class="mr-2">
+                                <i class="far fa-folder-open"></i>
+                            </span>
+                            <span>Sports</span>
+                            <p class="ml-auto font-normal">(7)</p>
+                        </a>
+                        <a href="#"
+                            class="flex leading-4 items-center text-gray-700 font-semibold text-sm uppercase transition hover:text-blue-500">
+                            <span class="mr-2">
+                                <i class="far fa-folder-open"></i>
+                            </span>
+                            <span>Technology</span>
+                            <p class="ml-auto font-normal">(17)</p>
+                        </a>
+                    </div>
+                </div> 
+                </div>
+  )
+}
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import Posts from "../../components/Posts/Posts";
+import Sidebar from "../../components/Sidebar/Sidebar";
+import "./home.css";
+import axios from "axios";
+import { useLocation } from "react-router";
+
+export default function Home() {
+  const [posts, setPosts] = useState([]);
+  const { search } = useLocation();
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await axios.get("/posts" + search);
+      setPosts(res.data);
+    };
+    fetchPosts();
+  }, [search]);
+  return (
+    <>
+      <div className="home">
+        <Posts posts={posts} />
+        <Sidebar />
+      </div>
+    </>
+  );
+}
